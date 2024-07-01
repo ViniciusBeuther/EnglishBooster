b035ec6f6b4815ba41f096e527622802742694a0
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-06-30 22:30:18 -0300

    New: Added a Modal
    Added a modal to edit a word

---
+++ src/Components/EditModal.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Button from './Button';
+
+interface EditModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  word: EnglishBooster | null;
+  onSave: (updatedWord: EnglishBooster) => void;
+}
+
+const EditModal: React.FC<EditModalProps> = ({ isOpen, onClose, word, onSave }) => {
+  const [englishWord, setEnglishWord] = useState(word?.english_word || '');
+  const [portugueseMeaning, setPortugueseMeaning] = useState(word?.portuguese_meaning || '');
+
+  const handleSave = () => {
+    if (word) {
+      onSave({
+        ...word,
+        english_word: englishWord,
+        portuguese_meaning: portugueseMeaning,
+      });
+      onClose();
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="dark fixed inset-0 flex items-center justify-center bg-opacity-50">
+      <div className="bg-[#1a1a1a] p-8 rounded-md shadow-md max-w-lg w-full">
+        <h2 className="text-2xl font-bolder mb-4 text-white">Editar Palavra</h2>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-white">Palavra em Inglês</label>
+          <input
+            type="text"
+            value={englishWord}
+            onChange={(e) => setEnglishWord(e.target.value)}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            placeholder={`${word?.english_word}`}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-white">Significado em Português</label>
+          <input
+            type="text"
+            value={portugueseMeaning}
+            onChange={(e) => setPortugueseMeaning(e.target.value)}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            placeholder={`${word?.portuguese_meaning}`}
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            onClick={onClose}
+            className="bg-red-500 text-white px-4 py-2 rounded-sm mr-2 hover:border-none border-none"
+          >
+            Cancelar
+          </button>
+
+          <Button text={"Salvar"} onClick={handleSave} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditModal;
--- src/Components/SavedWordsList.tsx
+++ src/Components/SavedWordsList.tsx
@@ -1,119 +1,167 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabase/supabase";
 import { Table, TableColumn, TableHeader, TableBody, TableRow, TableCell, Spinner, table } from "@nextui-org/react";
-import editIcon from "/edit.svg"
+import EditModal from "./EditModal";
+import editIcon from "/edit.svg";
 
 interface EnglishBooster {
-    id: number;
-    english_word: string;
-    portuguese_meaning: string;
+  id: number;
+  english_word: string;
+  portuguese_meaning: string;
 }
 
 interface TableColumnsTypes {
-    key: string;
-    label: string; // Use 'label' instead of 'Label'
+  key: string;
+  label: string;
 }
 
-const handleRemove = async ( ev:any, wordId:Number, setData:any, data:EnglishBooster[] ) => {
-    ev.preventDefault();
-    
-    try{
-        const { error } = await supabase
-        .from('englishBooster')
-        .delete()
-        .eq('id', `${wordId}`)
-        setData(data.filter(word => word.id !== wordId));
-    } 
-    catch( error ){
-        return console.log("error")
-    }
-            
-}
+const handleRemove = async (
+  ev: any,
+  wordId: Number,
+  setData: any,
+  data: EnglishBooster[]
+) => {
+  ev.preventDefault();
 
+  try {
+    const { error } = await supabase
+      .from("englishBooster")
+      .delete()
+      .eq("id", `${wordId}`);
+    setData(data.filter((word) => word.id !== wordId));
+  } catch (error) {
+    return console.log("error");
+  }
+};
 
 const SavedWordsList = () => {
-    const [data, setData] = useState<EnglishBooster[]>([]);
-    const [tableColumns, setTableColumns] = useState<TableColumnsTypes[]>([
-        {
-            key: "id",
-            label: "ID" // Use 'label' instead of 'Label'
-        },
-        {
-            key: "english_word",
-            label: "Palavra" // Correct the key to match the data
-        },
-        {
-            key: "portuguese_meaning",
-            label: "Significado" // Correct the key to match the data
-        },
-        {
-            key: "edit",
-            label: "Editar"
-        },
-        {
-            key: "remove",
-            label: "Excluir"
-        }
-    ]);
-    const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState<EnglishBooster[]>([]);
+  const [tableColumns, setTableColumns] = useState<TableColumnsTypes[]>([
+    {
+      key: "id",
+      label: "ID",
+    },
+    {
+      key: "english_word",
+      label: "Palavra",
+    },
+    {
+      key: "portuguese_meaning",
+      label: "Significado",
+    },
+    {
+      key: "edit",
+      label: "Editar",
+    },
+    {
+      key: "remove",
+      label: "Excluir",
+    },
+  ]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedWord, setSelectedWord] = useState<EnglishBooster | null>(
+    null
+  );
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                let { data: englishBooster, error } = await supabase
-                    .from<EnglishBooster>('englishBooster')
-                    .select('*');
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let { data: englishBooster, error } = await supabase
+          .from<EnglishBooster>("englishBooster")
+          .select("*");
 
-                if (error) throw error;
+        if (error) throw error;
 
-                setData(englishBooster?.sort() || []);
-            } catch (err) {
-                console.log("Um erro ocorreu...", err);
-            }
-        }
+        setData(englishBooster?.sort() || []);
+      } catch (err) {
+        console.log("Um erro ocorreu...", err);
+      }
+    }
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <>
-            <p>Palavras Salvas:</p>
-            {data && data.length > 0 ? (
-                <Table className="dark" aria-label="Tabela de palavras salvas">
-                    <TableHeader>
-                        {tableColumns.map((column) => (
-                            <TableColumn key={column.key}>{column.label}</TableColumn>
-                        ))}
-                    </TableHeader>
-                    <TableBody>
-                        { data.map((word) => (
-                            <TableRow key={word.id}>
-                                <TableCell>
-                                    { word.id }
-                                </TableCell>
-                                <TableCell>
-                                    { word.english_word }
-                                </TableCell>
-                                <TableCell>
-                                    { word.portuguese_meaning }
-                                </TableCell>
-                                <TableCell>
-                                    <img src={editIcon} className="text-white" alt="edit_icon" width={24} height={24} />
-                                </TableCell>
-                                <TableCell>
-                                    <button aria-label={`Remover a palavra ${word.english_word}`} className="bg-red-400 hover:border-red-500" onClick={(ev) => handleRemove(ev, word.id, setData, data)}>
-                                        X
-                                    </button>
-                                </TableCell>
-                            </TableRow>
-                        )) }
-                    </TableBody>
-                </Table>
-            ) : (
-                <Spinner size="md" color="secondary" />
-            )}
-        </>
-    );
+  const handleEdit = (word: EnglishBooster) => {
+    setSelectedWord(word);
+    setIsOpen(true);
+  };
+
+  const handleSave = async (updatedWord: EnglishBooster) => {
+    try {
+      const { error } = await supabase
+        .from("englishBooster")
+        .update(updatedWord)
+        .eq("id", updatedWord.id);
+
+      if (error) throw error;
+
+      setData(
+        data.map((word) => (word.id === updatedWord.id ? updatedWord : word))
+      );
+    } catch (error) {
+      console.log("Um erro ocorreu...", error);
+    }
+  };
+
+  const handleClose = (ev: any) => {
+    ev.preventDefault();
+    setIsOpen(false);
+    setSelectedWord(null);
+};
+
+  return (
+    <>
+      <p>Palavras Salvas:</p>
+      {data && data.length > 0 ? (
+        <Table className="dark" aria-label="Tabela de palavras salvas">
+          <TableHeader>
+            {tableColumns.map((column) => (
+              <TableColumn key={column.key}>{column.label}</TableColumn>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {data.map((word) => (
+              <TableRow key={word.id}>
+                <TableCell>{word.id}</TableCell>
+                <TableCell>{word.english_word}</TableCell>
+                <TableCell>{word.portuguese_meaning}</TableCell>
+                <TableCell>
+                  <button onClick={() => handleEdit(word)}>
+                    <img
+                      src={editIcon}
+                      className="text-white"
+                      alt="edit_icon"
+                      width={24}
+                      height={24}
+                    />
+                  </button>
+                </TableCell>
+                <TableCell>
+                  <button
+                    aria-label={`Remover a palavra ${word.english_word}`}
+                    className="bg-red-400 hover:border-red-500"
+                    onClick={(ev) =>
+                      handleRemove(ev, word.id, setData, data)
+                    }
+                  >
+                    X
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <Spinner size="md" color="secondary" />
+      )}
+      <EditModal
+        isOpen={isOpen}
+        onClose={(ev:any) => handleClose(ev)}
+        word={selectedWord}
+        onSave={handleSave}
+      />
+    </>
+  );
 };
 
 export default SavedWordsList;
