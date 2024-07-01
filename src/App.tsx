1613110d4fc2cb14460c3884b0d3ae24b8fef764
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-07-01 20:40:16 -0300

    Upd: Enter key

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,63 @@ import WordForm from "./Components/WordForm";
 import SavedWordsList from "./Components/SavedWordsList";
 import Practice from "./Components/Practice";
 
-
 const App = () => {
   const [selected, setSelected] = useState("Nova");
   let component: any = null;
-  
 
-  if( selected == "Nova" ){
+  if (selected == "Nova") {
     component = <WordForm />;
-  } 
-  else if ( selected == "Editar" ){
+  } else if (selected == "Editar") {
     component = <SavedWordsList />;
-  } else{
+  } else {
     component = <Practice />;
   }
-  
-  
-  return(
-    <div className=" p-8 rounded-xl shadow-lg bg-black w-[625px] text-center">
-      <h2>English Booster</h2>
+
+  return (
+    <div className=" p-8 rounded-xl shadow-lg bg-black w-[350px] md:w-[450px] lg:w-[625px] text-center">
+      <h2
+        className={`${
+          selected == "Praticar" ? "hidden" : "block"
+        } md:block lg:block`}
+      >
+        English Booster
+      </h2>
       <nav className="flex my-2 items-center justify-center">
-        <span className={`${selected == "Nova" ? "text-purple-500 font-bold bg-purple-200 rounded-lg py-1 px-2" : "text-white"} hover:cursor-pointer py-1 px-2`} onClick={() => setSelected("Nova")}>Nova</span>
-        <span className={`${selected == "Editar" ? "text-purple-500 font-bold bg-purple-200 rounded-lg py-1 px-2" : "text-white"} hover:cursor-pointer py-1 px-2`} onClick={() => setSelected("Editar")}>Editar</span>
-        <span className={`${selected == "Praticar" ? "text-purple-500 font-bold bg-purple-200 rounded-lg py-1 px-2" : "text-white"} hover:cursor-pointer py-1 px-2`} onClick={() => setSelected("Praticar")}>Praticar</span>
+        <span
+          className={`${
+            selected == "Nova"
+              ? "text-purple-500 font-bold bg-purple-200 rounded-lg py-1 px-2"
+              : "text-white"
+          } hover:cursor-pointer py-1 px-2`}
+          onClick={() => setSelected("Nova")}
+        >
+          Nova
+        </span>
+        <span
+          className={`${
+            selected == "Editar"
+              ? "text-purple-500 font-bold bg-purple-200 rounded-lg py-1 px-2"
+              : "text-white"
+          } hover:cursor-pointer py-1 px-2 hidden md:block lg:block`}
+          onClick={() => setSelected("Editar")}
+        >
+          Editar
+        </span>
+        <span
+          className={`${
+            selected == "Praticar"
+              ? "text-purple-500 font-bold bg-purple-200 rounded-lg py-1 px-2"
+              : "text-white"
+          } hover:cursor-pointer py-1 px-2`}
+          onClick={() => setSelected("Praticar")}
+        >
+          Praticar
+        </span>
       </nav>
-      
-        {component}
+
+      {component}
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/Components/Questionary.tsx
+++ src/Components/Questionary.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { supabase } from "../supabase/supabase";
 import { Spinner } from "@nextui-org/react";
 import WrongModal from "./WrongModal";
@@ -29,6 +29,7 @@ const Questionary:React.FC = () => {
 
     const checkAnswer = ( ev:any ) => {
         ev.preventDefault();
+        
         if ( answerValue.toLowerCase() == selectedWord.portuguese_meaning.toLowerCase() ){
             alert("Voce Acertou!");
             setAnswerValue("");
@@ -39,6 +40,10 @@ const Questionary:React.FC = () => {
             setAnswerValue("");
             randomizeWord(data);
         }
+    }
+
+    const handleKeyDown = (ev:React.KeyboardEvent<HTMLInputElement>) => {
+        ev.key == "Enter" ? checkAnswer(ev) : null;
     }
 
     useEffect(() => {
@@ -66,13 +71,13 @@ const Questionary:React.FC = () => {
         <>
         { data && data.length > 0 ? (
             <div>
-                <p className="text-5xl text-center text-purple-500 font-extrabold">{ selectedWord.english_word }</p>
+                <p className="text-4xl md:text-5xl lg:text-5xl text-center text-purple-500 font-extrabold">{ selectedWord.english_word }</p>
                 <div className="flex flex-col mt-5 gap-5">
                     <label htmlFor="answer" className="text-left">Qual é o significado?</label>
-                    <input type="text" name="answer_input" value={answerValue} onChange={(ev) => setAnswerValue(ev.target.value)} id="answer_input" className="py-2 px-4 rounded-lg" />
+                    <input type="text" name="answer_input" value={answerValue} onChange={(ev) => setAnswerValue(ev.target.value)} onKeyDown={(ev) => handleKeyDown(ev)} id="answer_input" className="py-2 px-4 rounded-lg" />
                     
                     <div className="flex w-full gap-5">
-                        <button className="bg-purple-500 w-full hover:border-purple-700 hover:bg-purple-600" onClick={(ev:any) => checkAnswer(ev)}>Verificar</button>
+                        <button className="bg-purple-500 w-full hover:border-purple-700 hover:bg-purple-600" onClick={(ev:any) => checkAnswer(ev)} onKeyDown={(ev:any) => checkAnswer(ev)} >Verificar</button>
                         <button className="bg-orange-500 w-full hover:border-orange-700 hover:bg-orange-600" onClick={(ev:any) => randomizeWord(data)}>Outra</button>
                     </div>
                 </div>
