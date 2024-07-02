e65932721d4e436b02d00fd05f2b37b46e33ac81
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-07-01 23:57:37 -0300

    Update WordForm.tsx

--- src/Components/WordForm.tsx
+++ src/Components/WordForm.tsx
@@ -25,7 +25,7 @@ const WordForm = () => {
         console.log("Data inserted successfully:", data);
       }
     }
-    
+  
     function handleSubmit(ev:any){
         ev.preventDefault()
         insertInDatabase( word, meaning );
